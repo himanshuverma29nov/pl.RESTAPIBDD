e5f3b339fc1c2fc2e7416557006e074b75a1f9f4
afzal.ahmed <afzalahmad92@gmail.com>
2021-12-24 12:05:02 +0530

    modfied test runner

--- target/classes/pl/rest/testRunners/NonDBTestRunner.java
+++ target/classes/pl/rest/testRunners/NonDBTestRunner.java
@@ -6,9 +6,7 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "classpath:features", glue = "classpath:pl.rest", tags = "(@Regression or @TestSet0) and (not @DB)",
-        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json",
-                "summary"},
-        monochrome = true
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
 )
 public class NonDBTestRunner extends AbstractTestNGCucumberTests {
 //    @Override
--- target/classes/pl/rest/testRunners/TestRunner.java
+++ target/classes/pl/rest/testRunners/TestRunner.java
@@ -6,9 +6,7 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "classpath:features", glue = "classpath:pl.rest", tags = "@Regression, @TestSet0",
-        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json",
-                "summary"},
-        monochrome = true
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 //    @Override
